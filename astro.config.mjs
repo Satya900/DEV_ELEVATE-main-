82a618b070249b93de1e70431dd5c23c3e7ed654
Satyabrata Mohanty <satyabrata.sot010109@pwioi.com>
2025-02-03 13:49:03 +0530

    ready to rule

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import partytown from '@astrojs/partytown';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
+  site: 'https://develevate.tech', // Replace with your actual site URL
   integrations: [
     tailwind(),
     partytown({
@@ -11,4 +15,9 @@ export default defineConfig({
       },
     }),
   ],
+  build: {
+    rollupOptions: {
+      external: ['@astrojs/rss', 'sanitize-html', 'marked']
+    }
+  }
 });
